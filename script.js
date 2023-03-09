9b5156fbf7cd4c372cb11cc0c45c3825d56aa3dd
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-09 22:38:30 +0100

    elaborazione for and js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /*VARIABILI*/
 
 /*COSTANTI*/
+const form = document.getElementById('form');
 
-const KmTravelled = form.querySelector('input[name ="età"]');
-const AgeUtente = form.querySelector('input[name = "km"]');
+const KmTravelled = form.querySelector('input[name ="km"]');
+const AgeUtente = form.querySelector('input[name = "età"]');
+
 
 let KmPrice = 0.21;
 let TotalTicketPrice = (KmTravelled * KmPrice);
@@ -13,18 +15,49 @@ let AdultDiscount = 0.40;
 let MinorDiscount = 0.20;
 
 /*MESSAGGIO*/
-let message = 'Prezzo del biglietto è: ';
+/*let messaggio = 'Prezzo del biglietto è: ';*/
+
+
+const btnCalcola = document.getElementById('Calculate');
+const btnCancella = document.getElementById('Cancel');
+
+const messaggio = document.querySelector('.messaggio');
 
 /*CONDIZIONALI*/
-if(AgeUtente < 18) {
-    message += (TotalTicketPrice - (TotalTicketPrice * MinorDiscount)).toFixed(2) +' &euro;';
-}
-if(AgeUtente > 65) {
-    message+= (TotalTicketPrice - (TotalTicketPrice * AdultDiscount)).toFixed(2) +' &euro;';
-}
-else if ((AgeUtente >18) && (AgeUtente < 65)) {
-    message += (TotalTicketPrice).toFixed(2) +' &euro;';
-}
-document.getElementById('messaggio').innerHTML =
 
- `<p>${message}</p>`;
+btnCalcola.addEventListener('click', function(){
+    let età = AgeUtente.value;
+    let km = KmTravelled.value;
+    let KmPrice = 0.21;
+    let TotalTicketPrice = km * KmPrice;
+    messaggio.innerHTML +=`<h2>${TotalTicketPrice} &euro;</h2>`;
+    messaggio.style.color = 'green';
+
+    let AdultDiscount = 0.40;
+    let MinorDiscount = 0.20;
+
+    if(AgeUtente < 18) {
+        messaggio += (TotalTicketPrice - MinorDiscount).toFixed(2) +' &euro;';
+    }
+    if(AgeUtente > 65) {
+        messaggio+= (TotalTicketPrice - AdultDiscount).toFixed(2) +' &euro;';
+    }
+    else if ((AgeUtente >18) && (AgeUtente < 65)) {
+        messaggio += (TotalTicketPrice).toFixed(2) +' &euro;';
+    }
+});
+btnCancella.addEventListener('click', function(){
+    AgeUtente.value= 0;
+    KmTravelled.value = 0;
+});
+
+let inputs = document.getElementsByClassName('form-input');
+for (var i =0; i< inputs.length; i++) {
+    inputs[i].addEventListener('keyup', function(){
+        if(this.value.length>=1) {
+            this.nextElementSibling.classList.add('fisso');
+        } else {
+            this.nextElementSibling.classList.remove('fisso');
+        }
+    });
+}
